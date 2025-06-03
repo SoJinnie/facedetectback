46932b837b6d133f55203c3b05d0fbe1a5d120d9
SoJinnie <ujj.adrienn.mariann@gmail.com>
2025-06-03 18:22:17 +0200

    Update server.js
    add line 30-35

--- server.js
+++ server.js
@@ -27,6 +27,12 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use((req, res, next) => {
+  console.log(`[${new Date().toISOString()}] ${req.method} ${req.url}`);
+  next();
+});
 
 
 app.get('/', (req, res) => {
